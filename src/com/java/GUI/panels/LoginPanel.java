20843b5b4a5f47a7b3c7551ed7ba7f0380f06e29
redex22 <n3xtfix@gmail.com>
2023-06-09 00:58:09 -0300

    Feat: Agrego validaciones para los campos de password y mail. Agrego cheque de coincidencias de password en interfaz y BDD.

--- src/com/java/GUI/panels/LoginPanel.java
+++ src/com/java/GUI/panels/LoginPanel.java
@@ -2,18 +2,22 @@ package com.java.GUI.panels;
 
 import java.awt.CardLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Cursor;
 import java.awt.Font;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+
+import com.services.UsuarioBeanRemote;
+
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.event.MouseAdapter;
@@ -33,7 +37,7 @@ public class LoginPanel extends JPanel {
 	/**
 	 * Create the panel.
 	 */
-	public LoginPanel(JPanel contentPane) {
+	public LoginPanel(JPanel contentPane, UsuarioBeanRemote usuarioBean) {
 		this.contentPane = contentPane;
 		setBackground(new Color(255, 255, 255));
 		lblMail = new JLabel("Correo:");
@@ -49,6 +53,56 @@ public class LoginPanel extends JPanel {
 		btnLogin = new JButton("Ingresar");
 		btnLogin.setBackground(new Color(125, 229, 251));
 		btnLogin.setForeground(new Color(40, 40, 40));
+		btnLogin.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// Implementar logica para ingreso a sistema
+				String email = txtFieldMail.getText();
+				char[] passwArr = passwordField.getPassword();
+				String passw = new String(passwArr);
+				
+				if(email.isEmpty() || passw.isEmpty()) {
+					JOptionPane.showMessageDialog(LoginPanel.this, "Por favor, ingrese un correo o contraseña.", "Login Error", JOptionPane.ERROR_MESSAGE);
+					passwordField.setText("");
+					return;
+				}
+				
+				if (!passw.matches("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+-=]).{8,}$")) {
+					// Muestra un mensaje de error si la contraseña no cumple con los requisitos mínimos
+					JOptionPane.showMessageDialog(null,
+							"Por favor ingrese una contraseña válida que contenga al menos una letra mayúscula, una letra minúscula, un número y un carácter especial, y tenga una longitud de al menos 8 caracteres.");
+					passwordField.setText("");
+					return;
+				}
+				
+				try {
+					InternetAddress correoInternet = new InternetAddress(email);
+					correoInternet.validate();
+				} catch (AddressException ex) {
+					// Muestra un mensaje de error si el correo electrónico no es válido
+					JOptionPane.showMessageDialog(null, "Por favor ingrese una dirección de correo electrónico válida.");
+					passwordField.setText("");
+					return;
+				}
+
+				String passwBDD = usuarioBean.selectPasswBy(email);
+				if(passwBDD == null) {
+					JOptionPane.showMessageDialog(LoginPanel.this, "Salió todo mal loco no podes", "Mal ahiiii makina", JOptionPane.INFORMATION_MESSAGE);
+					passwordField.setText("");
+					return;
+				}
+				else if(!passwBDD.equals(passw)) {
+					JOptionPane.showMessageDialog(LoginPanel.this, "El usuario y/o contraseña es incorrecto", "mal ahiiii makina", JOptionPane.YES_OPTION);
+					passwordField.setText("");
+					return;
+				} else {
+					JOptionPane.showMessageDialog(LoginPanel.this, "Salió todo bien loco bien ahí", "Bien ahiiii makina", JOptionPane.YES_OPTION);
+				}
+				txtFieldMail.setText("");
+				passwordField.setText("");
+				
+			}
+		});
 		
 		btnRegister = new JButton("Registrarse");
 		btnRegister.addMouseListener(new MouseAdapter() {
