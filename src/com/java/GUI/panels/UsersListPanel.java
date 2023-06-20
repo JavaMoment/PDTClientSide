a9613d5dca161a874ee4f8d4ceb922b13dd1a9b4
redex22 <n3xtfix@gmail.com>
2023-06-20 01:41:58 -0300

    Feat: Agrego botones de editar y eliminar usuarios. Agrego busqueda de usuarios por nombre 1 o 2 y boton de refrescar la tabla
    Los botones de editar y eliminar usuarios aun no son funcionales

--- src/com/java/GUI/panels/UsersListPanel.java
+++ src/com/java/GUI/panels/UsersListPanel.java
@@ -2,6 +2,8 @@ package com.java.GUI.panels;
 
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.RowFilter;
+
 
 import java.awt.Color;
 import java.awt.Cursor;
@@ -14,6 +16,8 @@ import net.miginfocom.swing.MigLayout;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JTable;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 import com.entities.Itr;
 import com.entities.Usuario;
@@ -25,8 +29,11 @@ import javax.swing.JTextField;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class UsersListPanel extends JPanel {
+
 	private JTable table;
 	private JTextField textFieldSearch;
 
@@ -34,11 +41,9 @@ public class UsersListPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public UsersListPanel(UsuarioBeanRemote usuarioBean, ItrBeanRemote itrBean) {
-		setLayout(new MigLayout("", "[89px][89px,grow]", "[][][23px,grow][][][][][][][][][][][]"));
+		setLayout(new MigLayout("", "[89px][176.00px,grow][]", "[][][23px,grow][][][][][][][][][][][]"));
 		
-		textFieldSearch = new JTextField();
-		add(textFieldSearch, "flowx,cell 1 0,alignx left,growy");
-		textFieldSearch.setColumns(10);
+		
 		
 		JLabel lblFilterTitle = new JLabel("Filtrar por:");
 		add(lblFilterTitle, "cell 0 1");
@@ -55,11 +60,16 @@ public class UsersListPanel extends JPanel {
 		List<Itr> itrs = itrBean.selectAll();
 		
 		table = new JTable();
-		table.setModel(new EntityTableModel<Usuario>(usersColNames, users));
+		TableModel usersTableModel = new EntityTableModel<>(usersColNames, users);
+		table.setModel(usersTableModel);
 		table.setColumnSelectionAllowed(true);
 		table.setCellSelectionEnabled(true);
 		scrollPane.setViewportView(table);
 		
+		textFieldSearch = new JTextField();
+		add(textFieldSearch, "flowx,cell 1 0,alignx left,growy");
+		textFieldSearch.setColumns(10);
+
 		JLabel lblUserType = new JLabel("Tipo de Usuario:");
 		add(lblUserType, "flowy,cell 0 2");
 		
@@ -92,6 +102,27 @@ public class UsersListPanel extends JPanel {
 		add(comboBoxStatus, "cell 0 2,growx");
 		
 		JButton btnSearch = new JButton("");
+		btnSearch.setToolTipText("Buscar por nombre");
+		btnSearch.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				String searchText = textFieldSearch.getText();
+				if(searchText.isBlank() || searchText.isEmpty()) {
+					return;
+				}
+				RowFilter<Object, Object> filter = new RowFilter<Object, Object>() {
+					public boolean include(Entry entry) {
+						String name1 = (String) entry.getValue(10);
+						String name2 = entry.getValue(11) != null ? (String) entry.getValue(11) : "";
+						return name1.toUpperCase().contains(searchText.toUpperCase()) || name2.contains(searchText);
+					}
+				};
+				TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(usersTableModel);
+				sorter.setRowFilter(filter);
+				table.setRowSorter(sorter);
+			}
+		});
 		btnSearch.setIcon(new ImageIcon(UsersListPanel.class.getResource("/com/java/resources/images/buscar.png")));
 		btnSearch.setFont(new Font("sansserif", 1, 12));
         btnSearch.setForeground(new Color(30, 122, 236));
@@ -99,6 +130,43 @@ public class UsersListPanel extends JPanel {
         btnSearch.setCursor(new Cursor(Cursor.HAND_CURSOR));
         btnSearch.setBorder(null);
 		add(btnSearch, "cell 1 0");
+		
+		JButton btnRefresh = new JButton("");
+		btnRefresh.setToolTipText("Refrescar");
+		btnRefresh.setIcon(new ImageIcon(UsersListPanel.class.getResource("/com/java/resources/images/actualizar.png")));
+		btnRefresh.setFont(new Font("sansserif", 1, 12));
+        btnRefresh.setForeground(new Color(30, 122, 236));
+        btnRefresh.setContentAreaFilled(false);
+        btnRefresh.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btnRefresh.setBorder(null);
+        btnRefresh.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(usersTableModel);
+				sorter.setRowFilter(null);
+				table.setRowSorter(sorter);
+			}
+		});
+		add(btnRefresh, "cell 1 0");
+		
+		JButton btnEditUser = new JButton("");
+		btnEditUser.setIcon(new ImageIcon(UsersListPanel.class.getResource("/com/java/resources/images/user.png")));
+		btnEditUser.setFont(new Font("sansserif", 1, 12));
+        btnEditUser.setForeground(new Color(30, 122, 236));
+        btnEditUser.setContentAreaFilled(false);
+        btnEditUser.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btnEditUser.setBorder(null);
+		add(btnEditUser, "flowx,cell 2 0");
+		
+		JButton btnDeleteUser = new JButton("");
+		btnDeleteUser.setIcon(new ImageIcon(UsersListPanel.class.getResource("/com/java/resources/images/delete-user.png")));
+		btnDeleteUser.setFont(new Font("sansserif", 1, 12));
+        btnDeleteUser.setForeground(new Color(30, 122, 236));
+        btnDeleteUser.setContentAreaFilled(false);
+        btnDeleteUser.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btnDeleteUser.setBorder(null);
+		add(btnDeleteUser, "cell 2 0");
+		
 
 	}
 
