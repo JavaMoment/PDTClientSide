7c1abe63c5a5c99774c3d9cf67f112416fdf6abe
redex22 <n3xtfix@gmail.com>
2023-06-29 01:42:46 -0300

    Feat: Agrego funcionalidad de filtrar por Tipo de Usuario/Itr/Activo-Inactivo

--- src/com/java/GUI/panels/UsersListPanel.java
+++ src/com/java/GUI/panels/UsersListPanel.java
@@ -9,7 +9,9 @@ import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Font;
 import java.time.Year;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import net.miginfocom.swing.MigLayout;
@@ -41,14 +43,13 @@ public class UsersListPanel extends JPanel {
 
 	private JTable table;
 	private JTextField textFieldSearch;
+	private HashMap<String, RowFilter<Object, Object>> filters = new HashMap<>();
 
 	/**
 	 * Create the panel.
 	 */
 	public UsersListPanel(UsuarioBeanRemote usuarioBean, ItrBeanRemote itrBean) {
 		setLayout(new MigLayout("", "[89px][176.00px,grow][]", "[][][23px,grow][][][][][][][][][][][]"));
-		
-		
 		
 		JLabel lblFilterTitle = new JLabel("Filtrar por:");
 		add(lblFilterTitle, "cell 0 1");
@@ -61,11 +62,12 @@ public class UsersListPanel extends JPanel {
 				.filter(value -> !value.equals("estudiantes") && !value.equals("tutores") && !value.equals("analistas")
 						&& !value.equals("contrasenia"))
 				.toArray(String[]::new);
+		String[] transientCols = {"tipoUsuario"};
 		
 		List<Itr> itrs = itrBean.selectAll();
 		
 		table = new JTable();
-		TableModel usersTableModel = new EntityTableModel<>(usersColNames, users);
+		TableModel usersTableModel = new EntityTableModel<>(usersColNames, users, transientCols);
 		table.setModel(usersTableModel);
 		table.setColumnSelectionAllowed(true);
 		table.setCellSelectionEnabled(true);
@@ -79,7 +81,7 @@ public class UsersListPanel extends JPanel {
 		add(lblUserType, "flowy,cell 0 2");
 		
 		String[] userTypes = {"Analista", "Estudiante", "Tutor"};
-		JComboBox comboBoxUserType = new JComboBox(userTypes);
+		JComboBox comboBoxUserType = new JComboBox(new DefaultComboBox(userTypes));
 		lblUserType.setLabelFor(comboBoxUserType);
 		add(comboBoxUserType, "cell 0 2,growx");
 		
@@ -88,14 +90,6 @@ public class UsersListPanel extends JPanel {
 		
 		JComboBox comboBoxItr = new JComboBox(new DefaultComboBox(itrs.toArray()));
 		lblItr.setLabelFor(comboBoxItr);
-		comboBoxItr.setSelectedIndex(0);
-		comboBoxItr.addItemListener(new ItemListener() {
-			@Override
-			public void itemStateChanged(ItemEvent e) {
-				// TO be implemented
-				return;
-			}
-		});
 		add(comboBoxItr, "cell 0 2,growx");
 		
 		JLabel lblGeneration = new JLabel("Generación:");
@@ -106,13 +100,118 @@ public class UsersListPanel extends JPanel {
 		spinnerYearGen.setValue(Year.now().getValue());
 		add(spinnerYearGen, "cell 0 2,growx");
 		
-		String[] status = {"Inactivo", "Activo"};
+		String[] status = {"0: Inactivo", "1: Activo"};
 		
 		JLabel lblStatus = new JLabel("Estado:");
 		add(lblStatus, "flowy,cell 0 2");
-		JComboBox comboBoxStatus = new JComboBox(status);
+		JComboBox comboBoxStatus = new JComboBox(new DefaultComboBox(status));
 		lblStatus.setLabelFor(comboBoxStatus);
 		add(comboBoxStatus, "cell 0 2,growx");
+
+		List<JComboBox> comboBoxes = List.of(comboBoxItr, comboBoxStatus, comboBoxUserType);
+		
+		comboBoxUserType.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if(comboBoxes.stream().allMatch(box -> box.getSelectedItem() == null)) {
+					filters.clear();
+					TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(usersTableModel);
+					sorter.setRowFilter(null);
+					table.setRowSorter(sorter);
+					return;
+				}
+				if(comboBoxUserType.getSelectedItem() == null & comboBoxes.stream().anyMatch(box -> box.getSelectedItem() != null)) {
+					filters.remove("userType");
+					RowFilter<Object, Object> filter = RowFilter.andFilter(filters.values());
+					TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(usersTableModel);
+					sorter.setRowFilter(filter);
+					table.setRowSorter(sorter);
+					return;
+				}
+				String selectedUserType = (String) comboBoxUserType.getSelectedItem();
+				RowFilter<Object, Object> userTypeFilter = new RowFilter<Object, Object>() {
+					public boolean include(Entry entry) {
+						int userTypeColIdx = table.getColumn("TipoUsuario").getModelIndex();
+						String userType = (String) entry.getValue(userTypeColIdx);
+						return userType.toUpperCase().equals(selectedUserType.toUpperCase());
+					}
+				};
+				filters.put("userType", userTypeFilter);
+				RowFilter<Object, Object> filter = RowFilter.andFilter(filters.values());
+				TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(usersTableModel);
+				sorter.setRowFilter(filter);
+				table.setRowSorter(sorter);
+			}
+		});
+		
+		comboBoxItr.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if(comboBoxes.stream().allMatch(box -> box.getSelectedItem() == null)) {
+					filters.clear();
+					TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(usersTableModel);
+					sorter.setRowFilter(null);
+					table.setRowSorter(sorter);
+					return;
+				}
+				if(comboBoxItr.getSelectedItem() == null & comboBoxes.stream().anyMatch(box -> box.getSelectedItem() != null)) {
+					filters.remove("itr");
+					RowFilter<Object, Object> filter = RowFilter.andFilter(filters.values());
+					TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(usersTableModel);
+					sorter.setRowFilter(filter);
+					table.setRowSorter(sorter);
+					return;
+				}
+				Itr selectedItr = (Itr) comboBoxItr.getSelectedItem();
+				RowFilter<Object, Object> itrFilter = new RowFilter<Object, Object>() {
+					public boolean include(Entry entry) {
+						int itrColIdx = table.getColumn("Itr").getModelIndex();
+						Itr itr = (Itr) entry.getValue(itrColIdx);
+						String itrName = itr.getNombre();
+						return itrName.toUpperCase().equals(selectedItr.getNombre().toUpperCase());
+					}
+				};
+				filters.put("itr", itrFilter);
+				RowFilter<Object, Object> filter = RowFilter.andFilter(filters.values());
+				TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(usersTableModel);
+				sorter.setRowFilter(filter);
+				table.setRowSorter(sorter);
+			}
+		});
+		
+		comboBoxStatus.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if(comboBoxes.stream().allMatch(box -> box.getSelectedItem() == null)) {
+					filters.clear();
+					TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(usersTableModel);
+					sorter.setRowFilter(null);
+					table.setRowSorter(sorter);
+					return;
+				}
+				if(comboBoxStatus.getSelectedItem() == null & comboBoxes.stream().anyMatch(box -> box.getSelectedItem() != null)) {
+					filters.remove("status");
+					RowFilter<Object, Object> filter = RowFilter.andFilter(filters.values());
+					TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(usersTableModel);
+					sorter.setRowFilter(filter);
+					table.setRowSorter(sorter);
+					return;
+				}
+				String selectedStatus = (String) comboBoxStatus.getSelectedItem();
+				RowFilter<Object, Object> statusFilter = new RowFilter<Object, Object>() {
+					public boolean include(Entry entry) {
+						int statusColIdx = table.getColumn("Activo").getModelIndex();
+						byte status = (byte) entry.getValue(statusColIdx);
+						return status == Byte.valueOf(selectedStatus.substring(0, 1));
+					}
+				};
+				filters.put("status", statusFilter);
+				RowFilter<Object, Object> filter = RowFilter.andFilter(filters.values());
+				TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(usersTableModel);
+				sorter.setRowFilter(filter);
+				table.setRowSorter(sorter);
+			}
+		});
 		
 		JButton btnSearch = new JButton("");
 		btnSearch.setToolTipText("Buscar por nombre");
@@ -126,9 +225,11 @@ public class UsersListPanel extends JPanel {
 				}
 				RowFilter<Object, Object> filter = new RowFilter<Object, Object>() {
 					public boolean include(Entry entry) {
-						String name1 = (String) entry.getValue(10);
-						String name2 = entry.getValue(11) != null ? (String) entry.getValue(11) : "";
-						return name1.toUpperCase().contains(searchText.toUpperCase()) || name2.contains(searchText);
+						int name1ColIdx = table.getColumn("Nombre1").getModelIndex();
+						int name2ColIdx = table.getColumn("Nombre2").getModelIndex();
+						String name1 = (String) entry.getValue(name1ColIdx);
+						String name2 = entry.getValue(name2ColIdx) != null ? (String) entry.getValue(11) : "";
+						return name1.toUpperCase().contains(searchText.toUpperCase()) || name2.toUpperCase().contains(searchText.toUpperCase());
 					}
 				};
 				TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(usersTableModel);
@@ -158,6 +259,7 @@ public class UsersListPanel extends JPanel {
 				TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(usersTableModel);
 				sorter.setRowFilter(null);
 				table.setRowSorter(sorter);
+				comboBoxes.stream().forEach(box -> box.setSelectedIndex(0));
 			}
 		});
 		add(btnRefresh, "cell 1 0");
@@ -175,21 +277,27 @@ public class UsersListPanel extends JPanel {
 				SwingWorker<String, Void> worker = new SwingWorker<String, Void>() {
 					@Override
                     protected String doInBackground() throws Exception {
-						int resultCode = JOptionPane.showConfirmDialog(UsersListPanel.this, "Elija un nombre de usuario que desee modificar.", "¡Atención!", JOptionPane.OK_CANCEL_OPTION);
+
+                        if(table.getSelectionModel().isSelectionEmpty()) {
+                        	int resultCode = JOptionPane.showConfirmDialog(UsersListPanel.this, "Elija un nombre de usuario que desee modificar.", "¡Atención!", JOptionPane.OK_CANCEL_OPTION);
+                        	if(resultCode == JOptionPane.CANCEL_OPTION) {
+                        		return null;
+                        	}
+                        }
 						
-						if(resultCode == JOptionPane.CANCEL_OPTION) {
-							return null;
-						}
 						while (table.getSelectedRow() < 0 || table.getSelectedColumn() < 0) {
                             Thread.sleep(100); // Wait para no sobreecargar cpu
                         }
-						// Cell selected, perform the desired action
-                        int selectedRow = table.getSelectedRow();
-                        int selectedColumn = table.getSelectedColumn();
-                        table.clearSelection();
-                        if(!table.getColumnName(selectedColumn).equals("NombreUsuario")) {
+						
+						int selectedRow = table.getSelectedRow();
+						int selectedColumn = table.getSelectedColumn();
+
+						if(!table.getColumnName(selectedColumn).equals("NombreUsuario")) {
+							table.clearSelection();
                         	return null;
                         }
+                        
+                        table.clearSelection();
                         String cellValue = table.getValueAt(selectedRow, selectedColumn).toString();
                         return cellValue;
 					}
@@ -221,6 +329,5 @@ public class UsersListPanel extends JPanel {
         btnDeleteUser.setBorder(null);
 		add(btnDeleteUser, "cell 2 0");
 		
-
 	}
 }
