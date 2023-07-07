8bcc47c2f7bd66c9058127c9ef21fbeab4a3442c
redex22 <n3xtfix@gmail.com>
2023-07-07 02:38:28 -0300

    Feat: Desarrollo de la ventana para los modulos de login y gestion de usuarios finalizada

--- src/com/java/GUI/Main.java
+++ src/com/java/GUI/Main.java
@@ -3,20 +3,15 @@ package com.java.GUI;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
-import java.awt.Cursor;
 import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JTabbedPane;
-import javax.swing.UIManager;
 
 import com.entities.Usuario;
 import com.java.GUI.panels.ContentHomePanel;
@@ -25,39 +20,21 @@ import com.java.GUI.panels.ContentPanel;
 import com.java.GUI.panels.HomePanel;
 import com.java.GUI.panels.LoginPanel;
 import com.java.GUI.panels.SignUpPanel;
-import com.java.GUI.panels.UserDataModificationPanel;
-import com.java.GUI.panels.UsersListPanel;
-import com.java.controller.BeansFactory;
-import com.java.enums.Beans;
-import com.services.AnalistaBeanRemote;
-import com.services.AreaBeanRemote;
-import com.services.DepartamentoBeanRemote;
-import com.services.EstudianteBeanRemote;
-import com.services.ItrBeanRemote;
-import com.services.LocalidadBeanRemote;
-import com.services.TutorBeanRemote;
-import com.services.UsuarioBeanRemote;
-
+import com.java.GUI.panels.UserManagementPanel;
+import com.java.GUI.panels.factory.UserPanelFactory;
+import com.java.GUI.panels.factory.UserPanelFactoryProvider;
 
 import java.awt.Toolkit;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 
+@SuppressWarnings("serial")
 public class Main extends JFrame {
 
 	private JPanel contentPane;
 	private JPanel loginPanel;
 	private JPanel signupPanel; 
 	private Usuario user;
-	private static UsuarioBeanRemote usuarioBean;
-	private static DepartamentoBeanRemote depaBean;
-	private static LocalidadBeanRemote localidadBean;
-	private static ItrBeanRemote itrBean;
-	private static AnalistaBeanRemote analiBean;
-	private static EstudianteBeanRemote estudBean;
-	private static AreaBeanRemote areaBean;
-	private static TutorBeanRemote tutorBean;
+	private UserPanelFactory userPanelFactory;
 
 	/**
 	 * Launch the application.
@@ -66,14 +43,6 @@ public class Main extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					usuarioBean = BeansFactory.getBean(Beans.Usuario, UsuarioBeanRemote.class);
-					depaBean = BeansFactory.getBean(Beans.Departamentos, DepartamentoBeanRemote.class);
-					localidadBean = BeansFactory.getBean(Beans.Localidades, LocalidadBeanRemote.class);
-					itrBean = BeansFactory.getBean(Beans.Itr, ItrBeanRemote.class);
-					analiBean = BeansFactory.getBean(Beans.Analista, AnalistaBeanRemote.class);
-					estudBean = BeansFactory.getBean(Beans.Estudiante, EstudianteBeanRemote.class);
-					areaBean = BeansFactory.getBean(Beans.Area, AreaBeanRemote.class);
-					tutorBean = BeansFactory.getBean(Beans.Tutor, TutorBeanRemote.class);
 					Main frame = new Main();
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -94,8 +63,7 @@ public class Main extends JFrame {
 		setLocationRelativeTo(null);
 		setExtendedState(MAXIMIZED_BOTH);
 		setTitle("UTEC ERP");
-		initUserMngmnt();
-		//initLogin();
+		initLogin();
 	}
 	
 	public void initLogin() {
@@ -113,8 +81,8 @@ public class Main extends JFrame {
 		CardLayout cardLayout = new CardLayout();
 
 		JPanel cardPanel = new JPanel(cardLayout);
-		loginPanel = new LoginPanel(cardPanel, usuarioBean);
-		signupPanel = new SignUpPanel(cardPanel, usuarioBean, depaBean, localidadBean, itrBean, analiBean, estudBean, areaBean, tutorBean);
+		loginPanel = new LoginPanel(cardPanel);
+		signupPanel = new SignUpPanel(cardPanel);
 		JScrollPane scrollPane = new JScrollPane(signupPanel);
 		cardPanel.add(loginPanel, "login");
 		cardPanel.add(scrollPane, "signup");
@@ -134,7 +102,7 @@ public class Main extends JFrame {
 		setContentPane(contentPane);
 		
 		ContentHomePanel homePanelContent = new ContentHomePanel();
-		HomePanel homePanel = new HomePanel();
+		HomePanel homePanel = (HomePanel) userPanelFactory.createHomePanel();
 		
 		CardLayout cardLayout = new CardLayout();
         JPanel cardPanel = new JPanel(cardLayout);
@@ -164,40 +132,20 @@ public class Main extends JFrame {
 		setContentPane(contentPane);
 		getContentPane().setLayout(new CardLayout(0, 0));
 		
-		JTabbedPane tabs = new JTabbedPane(JTabbedPane.TOP);
+		UserManagementPanel tabs = userPanelFactory.createUserManagement(user);
 		getContentPane().add(tabs, "userManagement");
 		
-		JButton logout = new JButton ();
-		logout.setIcon(new ImageIcon(HomePanel.class.getResource("/com/java/resources/images/icons8-go-back-16.png")));
-		logout.setBackground(new Color(125, 229, 251));
-		logout.setForeground(new Color(40, 40, 40));
-		logout.setContentAreaFilled(false);
-		logout.setBorder(null);
-		logout.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		logout.setToolTipText("Click aquí para volver hacia el menú de Home");
-	    logout.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mousePressed(MouseEvent e) {
-		        initHome();
-				revalidate();
-			}
-		});
-
-	    tabs.addTab("", null, new JPanel(), null);
-	    
-	    tabs.setTabComponentAt(tabs.getTabCount() - 1, logout);
-		
-		JPanel usersListPanel = new UsersListPanel(usuarioBean, itrBean);
-		tabs.addTab("Listado de usuarios", null, usersListPanel, null);
-		
-		JPanel userDataModPanel = new UserDataModificationPanel();
-		JScrollPane scrollPane = new JScrollPane(userDataModPanel);
-		tabs.addTab("Modificación de datos de usuario/s", null, scrollPane, null);
-		
-		JPanel panel4 = new JPanel();
-		tabs.addTab("Modificación de datos propios", null, panel4, null);
-		
-		tabs.setSelectedIndex(1);
 	}
 
+	public void setUser(Usuario user) {
+		this.user = user;
+	}
+	
+	public Usuario getUser() {
+		return user;
+	}
+	
+	public void setUserPanelFactory(Usuario user) {
+		this.userPanelFactory = UserPanelFactoryProvider.getUserPanel(user);
+	}
 }
