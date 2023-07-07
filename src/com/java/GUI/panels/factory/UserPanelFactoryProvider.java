5bb34598e436c537550b0b17734260c389a22ace
redex22 <n3xtfix@gmail.com>
2023-07-07 02:38:05 -0300

    Feat: Creo paquete factory para la creacion de paneles segun tipo de usuario

---
+++ src/com/java/GUI/panels/factory/UserPanelFactoryProvider.java
@@ -0,0 +1,18 @@
+package com.java.GUI.panels.factory;
+
+import java.util.Map;
+
+import com.entities.Usuario;
+
+public class UserPanelFactoryProvider {
+	
+	private final static Map<String, UserPanelFactory> userTypePanels = Map.of(
+			"Analista", new AnalystPanelFactory(),
+			"Estudiante", new StudentPanelFactory()
+			);
+	
+	public static UserPanelFactory getUserPanel(Usuario user) {
+		return userTypePanels.get(user.getTipoUsuario());
+	}
+
+}
