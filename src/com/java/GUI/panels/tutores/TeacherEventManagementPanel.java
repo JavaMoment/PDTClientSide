bb4d0c312d61de8cad3d112d57c5de75b17d10ad
redex22 <n3xtfix@gmail.com>
2023-07-08 04:37:42 -0300

    Fix: Arreglo vista profesor

--- src/com/java/GUI/panels/tutores/TeacherEventManagementPanel.java
+++ src/com/java/GUI/panels/tutores/TeacherEventManagementPanel.java
@@ -1,16 +1,26 @@
 package com.java.GUI.panels.tutores;
 
+import com.java.GUI.panels.AuxiliaryListofEventStatesPanel;
+import com.java.GUI.panels.CreateEventPanel;
+import com.java.GUI.panels.EventModeAuxiliaryListPanel;
 import com.java.GUI.panels.EventsPanel;
+import com.java.GUI.panels.ListEventPanel;
+import com.java.GUI.panels.ModifyCallsEventPanel;
+import com.java.GUI.panels.ModifyEventPanel;
+import com.java.GUI.panels.RegisterAssistsEventPanel;
+import com.java.GUI.panels.RegisterCallsEventPanel;
+import com.java.GUI.panels.RemoveEventPanel;
 
 public class TeacherEventManagementPanel extends EventsPanel {
 
 	public TeacherEventManagementPanel() {
 		super();
 		super.remove(2);
-		super.remove(4);
-		super.remove(3);
 		super.remove(2);
-		super.remove(4);
+		super.remove(2);
+		super.remove(2);
+		super.remove(2);
+		super.remove(3);
 		super.remove(3);
 	}
 
